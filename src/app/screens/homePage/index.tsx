35678dea29f702c1bdb59badbd44a08bb20259b6
obabakir <bubufighting01@gmail.com>
2026-06-28 17:19:37 +0900

    fix: organise homepage related sectional componets

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,5 +1,22 @@
+import React from "react";
+
 import { Container } from "@mui/material";
+import Statistics from "./Statistics";
+import PopularDishes from "./PopularDishes";
+import NewDishes from "./NewDishes";
+import Advertisement from "./Advertisement";
+import ActiveUsers from "./ActiveUsers";
+import Events from "./Events";
 
 export default function HomePage() {
-  return <Container>HomePage</Container>;
+  return (
+    <Container>
+      <Statistics />
+      <PopularDishes />
+      <NewDishes />
+      <Advertisement />
+      <ActiveUsers />
+      <Events />
+    </Container>
+  );
 }
