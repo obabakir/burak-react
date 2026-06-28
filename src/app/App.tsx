3bb34af58e94aa04dc4d092f0ce68084bf0fa719
obabakir <bubufighting01@gmail.com>
2026-06-28 14:17:55 +0900

    feat: integration footer and basket source

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,18 +2,20 @@ import React from "react";
 
 import { Link, Route, Switch, useLocation } from "react-router-dom";
 
-import { HomePage } from "./screens/homePage";
-import { ProductsPage } from "./screens/productsPage";
-import { OrdersPage } from "./screens/ordersPage";
-import { UserPage } from "./screens/userPage";
-import { Footer } from "./components/footer";
-import { HomeNavbar } from "./components/headers/HomeNavbar";
-import { OtherNavbar } from "./components/headers/OtherNavbar";
+import HomePage from "./screens/homePage";
+import ProductsPage from "./screens/productsPage";
+import OrdersPage from "./screens/ordersPage";
+import UserPage from "./screens/userPage";
+import Footer from "./components/footer";
+import HomeNavbar from "./components/headers/HomeNavbar";
+import OtherNavbar from "./components/headers/OtherNavbar";
 // @ts-ignore: side-effect import of CSS without type declarations
 import "../css/app.css";
 // @ts-ignore: side-effect import of CSS without type declarations
 import "../css/navbar.css";
-import { HelpPage } from "./screens/helpPage";
+// @ts-ignore: side-effect import of CSS without type declarations
+import "../css/footer.css";
+import HelpPage from "./screens/helpPage";
 
 function App() {
   const location = useLocation();
--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -1,7 +1,8 @@
 import { Box, Button, Container, Stack } from "@mui/material";
 import { Link, NavLink } from "react-router-dom";
+import Basket from "./Basket";
 
-export function HomeNavbar() {
+export default function HomeNavbar() {
   const authMember = null;
   return (
     <div className="home-navbar">
@@ -48,7 +49,8 @@ export function HomeNavbar() {
                 Help{" "}
               </NavLink>
             </Box>
-            {/*Basket*/}
+            {/* Basket.txs fileni chaqirdik */}
+            <Basket />
             {!authMember ? (
               <Box>
                 <Button variant="contained" className="login-button">
